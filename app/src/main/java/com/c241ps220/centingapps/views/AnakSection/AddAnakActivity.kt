c66317f4f17cdb9e137288e03a9016b132582225
ryandhikabaa <ryandhikabintang0@gmail.com>
2024-06-08 15:26:16 +0700

    add anak

--- app/src/main/java/com/c241ps220/centingapps/views/AnakSection/AddAnakActivity.kt
+++ app/src/main/java/com/c241ps220/centingapps/views/AnakSection/AddAnakActivity.kt
@@ -3,6 +3,7 @@ package com.c241ps220.centingapps.views.AnakSection
 import android.app.DatePickerDialog
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,9 @@ import java.util.Locale
 class AddAnakActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddAnakBinding
+    private var isSelectedGender = 0f // 0 Laki, 1 Perempuan
+    private var isSelectedHeightBirth = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +31,56 @@ class AddAnakActivity : AppCompatActivity() {
 
         setupToolbar()
 
+        setupGender()
+        setupSeekBar()
+
         with(binding){
             etBirthChild.setOnClickListener { showDatePickerDialog() }
+
+            btSave.setOnClickListener {
+                finish()
+            }
 
         }
 
     }
+
+    private fun setupGender(){
+        with(binding){
+            divGenderLaki.setOnClickListener {
+                isSelectedGender = 0f // "Laki-Laki"
+                divGenderLaki.setBackgroundResource(R.drawable.rectangle_stroke2)
+                divGenderPerempuan.setBackgroundResource(R.drawable.rectangle_stroke_transparent)
+            }
+            divGenderPerempuan.setOnClickListener {
+                isSelectedGender = 1f // "Perempuan"
+                divGenderLaki.setBackgroundResource(R.drawable.rectangle_stroke_transparent)
+                divGenderPerempuan.setBackgroundResource(R.drawable.rectangle_stroke2)
+            }
+        }
+    }
+
+    private fun setupSeekBar(){
+        with(binding){
+
+            sbHeightBirth.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    tvValueHeightBirth.setText("$progress Cm")
+                    isSelectedHeightBirth = progress.toFloat()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                    var progress = seekBar.progress
+                    tvValueHeightBirth.setText("$progress Cm")
+                    isSelectedHeightBirth = progress.toFloat()
+                }
+            })
+        }
+    }
+
 
     private fun setupToolbar(){
         with(binding.divToolbar){
